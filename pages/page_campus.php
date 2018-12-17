7c9f623a979732353283588dcd4623e55dde8be0
simonbourrasseau <45590458+simonbourrasseau@users.noreply.github.com>
2018-12-17 14:29:51 +0100

    Add files via upload

--- pages/page_campus.php
+++ pages/page_campus.php
@@ -6,23 +6,62 @@
  * Time: 10:13
  */
 
-class page_campus
-{
-
-}
-
 ?><!DOCTYPE html>
 <html lang="Fr-fr">
 <head>
     <link rel="stylesheet" type="text/css" href="../style/campus.css">
     <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../theme.css">
 	<title>campus</title>
 </head>
 <body>
-<div class="corp">
-    <div class="nantes">
+<ul id="menu-accordeon">
+    <li><a href="#">Menu</a>
+        <ul>
+            <li><a href="#">Espace Membre</a></li>
+            <li><a href="#">Paramètre</a></li>
+            <li><a href="#">Historique</a></li>
+            <li><a href="#">Mes articles</a></li>
+            <li><a href="#">Deconnexion</a></li>
+        </ul>
+</ul>
+<a href="accueil.html">
+    <img src="https://images.ecosia.org/IDmGrxFc9WpZaW3ETo-47MPAqnk=/0x390/smart/http%3A%2F%2Fwww.lemansdeveloppement.fr%2Fwp-content%2Fuploads%2F2015%2F06%2FLOGO_IMIE_H.jpg" alt="LOGO IMIE" id="LOGO" width="30%" height="30%">
+</a>
+<div><center><ul id="nav">
+            <li id="nav-home"><a href="#">Accueil</a>
+            </li>
+            <li id="nav-it_start"><a href="#">IT START</a></li>
+            <li id="nav-dev"><a href="#">DEV</a></li>
+            <li id="nav-ops"><a href="#">OPS</a></li>
+            <li id="nav-digi"><a href="#">DIGI</a></li>
+            <li id="nav-staff"><a href="#">staff</a></li>
+            <li id="nav-contact"><a href="#">Contact</a></li>
+            <li id="searchbar">
+
+                <form action="" class="formulaire">
+                    <input class="champ" type="text" value="Recherche...)"/>
+                    <input class="bouton" type="button" value= "entrer" />
+
+                </form>
+            </li>
+        </ul></center></div>
+
+<h4 id="pua"><a href="#"> Proposer un article </a></h4>
+</br></br></br>
+
+<div id="CGU">
+
+    <center>
+        <p id="CGU">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse facilisis quis ante sodales vehicula. Pellentesque hendrerit elit orci, ac ultricies nunc bibendum ac. Integer volutpat, dolor bibendum consectetur maximus, leo tellus posuere ipsum, eget fermentum sapien quam quis nibh. Mauris malesuada posuere arcu sit amet blandit. Praesent a porttitor massa. Sed iaculis dignissim dolor. Suspendisse purus velit, efficitur ac nunc condimentum, imperdiet malesuada eros. Vestibulum vel justo eu nisi egestas sollicitudin facilisis faucibus felis. Fusce consequat est enim, ac eleifend nisl tempus a. Phasellus ultrices a nunc non vulputate. Maecenas hendrerit eu neque eu vestibulum. Ut vitae ex scelerisque, pretium diam a, vehicula purus. Aliquam aliquam sollicitudin dapibus. Pellentesque eget ex egestas, sodales eros vitae, sodales augue. Vestibulum suscipit elementum nibh, at ullamcorper enim. Phasellus in odio at tortor pulvinar egestas.</p>
+    </center>
+</div>
+</a>
+<div>
+    <table>
+    <tr><td><div class="nantes">
         <h1 align="center">Nantes</h1>
-        <p>Coordonnées
+        <p>Coordonnées<br>
             213 route de Rennes – 44700 ORVAULT <br>
             02 28 01 37 54 / nantes@imie.fr</p>
         <p>Le campus de Nantes en bref : <br>
@@ -31,72 +70,74 @@ class page_campus
             Accès 24h/24 <br>
             Fibre 300 Mbs <br>
             Salle serveur dédiée formation</p>
-    </div>
+    </div></td>
 
 
-    <div class="angers">
-        <h1>Angers</h1>
-        <p>Coordonnées
-            Centre Pierre Cointreau – 132 Avenue de Lattre de Tassigny – 49000 Angers
+    <td><div class="angers">
+        <h1 align="center">Angers</h1>
+        <p>Coordonnées<br>
+            Centre Pierre Cointreau – 132 Avenue de Lattre de Tassigny – 49000 Angers<br>
             02 41 66 67 66 / angers@imie.fr</p>
-        <p> Le campus d’Angers en bref
-            11 salles de cours
-            Espace dédié à la réalité virtuelle
-            Salle serveur
+        <p> Le campus d’Angers en bref<br>
+            11 salles de cours<br>
+            Espace dédié à la réalité virtuelle<br>
+            Salle serveur<br>
             Salle de repos et babyfoot</p>
-    </div>
+    </div></td>
 
 
-    <div class="caen">
-        <h1>Caen</h1>
-        <p>Coordonnées
-            10 Place François Mitterrand – 14200 Hérouville-St-Clair
+    <td><div class="caen">
+        <h1 align="center">Caen</h1>
+        <p>Coordonnées<br>
+            10 Place François Mitterrand – 14200 Hérouville-St-Clair<br>
             02 53 35 74 20 / caen@imie.fr</p>
-        <p> Le campus de Caen en bref
-            6 salles de cours
-            2 espaces de vie
+        <p> Le campus de Caen en bref<br>
+            6 salles de cours<br>
+            2 espaces de vie<br>
             1 espace de coworking</p>
-    </div>
+    </div></td>
+    </tr>
 
 
-    <div class="paris">
-        <h1>Paris</h1>
-        <p>Coordonnées
-            70 rue Marius Aufan 92300 Levallois-Perret
+    <tr><td><div class="paris">
+        <h1 align="center">Paris</h1>
+        <p>Coordonnées<br>
+            70 rue Marius Aufan 92300 Levallois-Perret<br>
             01 41 05 73 80 / paris@imie.fr </p>
         <p> Le campus de Paris en bref :
-            10 salles de cours
-            1 Open Space connecté
+            10 salles de cours<br>
+            1 Open Space connecté<br>
             1 Cafétéria et de nombreux commerces à proximité</p>
-    </div>
+    </div></td>
 
 
-    <div class="st_naze">
-        <h1>St Nazaire</h1>
-        <p> Coordonnées
-            89 Avenue de la République – 44600 Saint-Nazaire
-            02 28 01 37 54 / saint-nazaire@imie.fr
+    <td><div class="st_naze">
+        <h1 align="center">St Nazaire</h1>
+        <p> Coordonnées<br>
+            89 Avenue de la République – 44600 Saint-Nazaire<br>
+            02 28 01 37 54 / saint-nazaire@imie.fr<br>
         </p>
-        <p> Le campus de Saint-Nazaire en bref :
-            3 salles
-            1 espace coworking
-            Accès 24h/24
+        <p> Le campus de Saint-Nazaire en bref :<br>
+            3 salles<br>
+            1 espace coworking<br>
+            Accès 24h/24<br>
             Fibre </p>
-    </div>
+    </div></td>
 
 
-    <div class="renne">
-        <h1>Renne</h1>
-        <p> Coordonnées
-            Campus de Ker Lann – Rue de Maupertuis – 35170 BRUZ
-            02 23 44 69 00 / rennes@imie.fr
+    <td><div class="rennes">
+        <h1 align="center">Rennes</h1>
+        <p> Coordonnées<br>
+            Campus de Ker Lann – Rue de Maupertuis – 35170 BRUZ<br>
+            02 23 44 69 00 / rennes@imie.fr<br>
         </p>
-        <p> Le campus de Rennes en bref :
-            11 salles de cours
-            Amphithéâtre
-            Espace dédié à la réalité virtuelle
-            Salle de détente et Babyfoot</p>
-    </div>
+        <p> Le campus de Rennes en bref :<br>
+            11 salles de cours<br>
+            Amphithéâtre<br>
+            Espace dédié à la réalité virtuelle<br>
+            Salle de détente et Babyfoot</p><br>
+    </div></td></tr>
+    </table>
 </div>
 </body>
 </html>
